25691237e7398bef35f9e632588be0e1ea8b3fc1
Egor Serikov <103260426+serikovlearning@users.noreply.github.com>
2025-06-06 00:41:13 +0600

    Update telescope.lua

--- nvim/lua/serikov/telescope.lua
+++ nvim/lua/serikov/telescope.lua
@@ -1,110 +1,74 @@
-local M = { 'nvim-telescope/telescope.nvim', dependencies = { 'nvim-lua/plenary.nvim' } }
-local MB = { "nvim-telescope/telescope-file-browser.nvim", dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" } }
--- local MS = { "nvim-telescope/telescope-ui-select.nvim" }
-
-local function vsplit_and_callback(fn)
-    vim.cmd("only")
-    vim.cmd("vsplit")
-    vim.cmd("wincmd L")
-    fn()
-end
-
-local keymap = vim.keymap
+local M = {
+    "nvim-neotest/neotest",
+    dependencies = {
+        "nvim-neotest/nvim-nio",
+        "nvim-lua/plenary.nvim",
+        "antoinemadec/FixCursorHold.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "marilari88/neotest-vitest",
+        "nvim-neotest/neotest-jest",
+    }
+}
 
 function M.config()
-    local telescope = require('telescope')
-    local telescope_builtin = require('telescope.builtin')
-    -- telescope.load_extension("telescope-ui-select")
-    --
-    local telescope_themes = require("telescope.themes")
-    telescope.setup({
-        defaults = {
-            -- prompt_position = 'top',
-            -- layout_strategy = 'horizontal',
-            sorting_strategy = 'ascending',
-            -- use_less = false
-        },
-        pickers = {
-            find_files = { hidden = true }
-        },
-        extensions = {
-            -- ["ui-select"] = {
-            --     telescope_themes.get_cursor {
-            --     }
-            -- },
-            ["file_browser"] = {
-                grouped = true,
-                respect_gitignore = false,
-                mappings = {
-                    ["i"] = {
-                        ["<C-l>"] = function(prompt_bufnr)
-                            local action_state = require("telescope.actions.state")
-                            local actions = require("telescope.actions")
-                            local entry = action_state.get_selected_entry()
-                            actions.close(prompt_bufnr)
-                            vsplit_and_callback(function()
-                                vim.cmd("edit " .. vim.fn.fnameescape(entry.path))
-                            end)
-                        end,
-                    },
-                    ["n"] = {
-                        ["<C-l>"] = function(prompt_bufnr)
-                            local action_state = require("telescope.actions.state")
-                            local actions = require("telescope.actions")
-                            local entry = action_state.get_selected_entry()
-                            actions.close(prompt_bufnr)
-                            vsplit_and_callback(function()
-                                vim.cmd("edit " .. vim.fn.fnameescape(entry.path))
-                            end)
-                        end,
-                    },
-                },
-            }
+    local neotest = require('neotest')
+    local jest = require("neotest-jest")
+
+    neotest.setup({
+        -- adapters = {
+        --     jest({
+        --         jestCommand = "npm test --",
+        --         cwd = function(path)
+        --             return vim.fn.getcwd()
+        --         end,
+        --     }),
+        -- },
+        adapters = {
+            require('neotest-jest')({
+                jestCommand = "npm run test --",
+                -- jestConfigFile = "custom.jest.config.ts",
+                env = { CI = true },
+                cwd = function(path)
+                    return vim.fn.getcwd()
+                end,
+            }),
         }
     })
-    -- telescope.load_extension("ui-select")
-    telescope.load_extension("file_browser")
-    keymap.set(
-        'n',
-        '<leader><leader>',
-        function()
-            telescope_builtin.find_files()
-        end,
-        { desc = 'Telescope find files' }
-    )
-    keymap.set(
-        'n',
-        '<leader>ff',
-        function()
-            telescope_builtin.live_grep()
-        end,
-        { desc = 'Telescope find files' }
-    )
-    local call_with_normal = require("additional.utils").call_with_normal
-    keymap.set(
-        "n",
-        "<leader>e",
-        call_with_normal(
-            function()
-                telescope.extensions.file_browser.file_browser({
-                    path = vim.fn.expand("%:p:h"),
-                    select_buffer = true
-                })
-            end
-        )
-    )
+
+
+    local keymap = vim.keymap
 
     keymap.set(
         "n",
-        "<leader>gs",
-        call_with_normal(
-            function()
-                telescope_builtin.git_status()
-            end
-        )
+        "<leader>nt",
+        function()
+            -- neotest.run.run()
+            neotest.run.run(vim.fn.expand('%'))
+        end
+    )
+    keymap.set(
+        "n",
+        "<leader>ns",
+        function()
+            neotest.summary.toggle()
+        end
+    )
+    keymap.set(
+        "n",
+        "<leader>na",
+        function()
+            -- neotest.watch.toggle(vim.fn.expand("%"))
+            neotest.run.run({ suite = true })
+        end
+    )
+    keymap.set(
+        "n",
+        "<leader>nw",
+        function()
+            -- neotest.watch.toggle(vim.fn.expand("%"))
+            neotest.watch.toggle()
+        end
     )
 end
 
--- return { MS, M, MB }
-return {  M, MB }
--- return vim.tbl_extend("force", M, MB)
+return M
