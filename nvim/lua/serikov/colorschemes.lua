20f270f3ffe0ec06302adfe2d7ff2cab171cf6e9
Egor Serikov <103260426+serikovlearning@users.noreply.github.com>
2025-06-06 00:39:37 +0600

    Update colorschemes.lua

--- nvim/lua/serikov/colorschemes.lua
+++ nvim/lua/serikov/colorschemes.lua
@@ -9,13 +9,13 @@ function M.config()
             light = "latte",
             dark = "mocha",
         },
-        transparent_background = true, -- disables setting the background color.
+        transparent_background = false, -- disables setting the background color.
         show_end_of_buffer = false,    -- shows the '~' characters after the end of buffers
         term_colors = false,           -- sets terminal colors (e.g. `g:terminal_color_0`)
         dim_inactive = {
             enabled = true,            -- dims the background color of inactive window
             shade = "dark",
-            percentage = 0.15,         -- percentage of the shade to apply to the inactive window
+            percentage = 0.85,         -- percentage of the shade to apply to the inactive window
         },
         no_italic = false,             -- Force no italic
         no_bold = false,               -- Force no bold
